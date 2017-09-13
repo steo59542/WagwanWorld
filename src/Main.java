d55333bcb571ca07513ceeadc0d56da45bf41e6f
Sam Teo <steo.59542@farnborough.ac.uk>
2017-09-13 13:55:26 +0100

    Added comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Wagwan world!");
+        System.out.println("Wagwan world!"); // This is a thing
     }
 }
